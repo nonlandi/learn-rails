eb59ca1f2f51cc5a7281389d011b0e81ce45cc99
Martin Landl <ivansik42+dev@gmail.com>
2013-11-23 16:46:49 +0100

    Update production environment settings for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ LearnRails::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
@@ -77,4 +78,21 @@ LearnRails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # email enabled in production
+config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = false
+config.action_mailer.default :charset => "utf-8"
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["DOMAIN_NAME"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+}
+
 end
